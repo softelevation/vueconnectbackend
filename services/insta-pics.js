218162eaa152a26cf58000d68b99ebd516d41b0e
Pawan <pawan@localhost.com>
2020-10-29 22:23:27 +0530

    added log

--- services/insta-pics.js
+++ services/insta-pics.js
@@ -2,8 +2,9 @@ const axios = require('axios');
 
 exports.getPics = async (req, res) => {
     const { id } = req.params;
-    const response = await axios.get(`https://www.instagram.com/${id}/?__a=1`);
     try{
+        const response = await axios.get(`https://www.instagram.com/${id}/?__a=1`);
+        console.log("response.data",response.data);
         const picUrl=response.data['graphql']['user']['profile_pic_url_hd'] 
         const fileRes= await axios({
                                     url:picUrl,
